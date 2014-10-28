0897225cf277ef523d213055945875b1137e45b9
Eric Fournier <me@ericdfournier.com>
2014-10-28 12:05:02 -0700

    Added skeleton of fillsink comparison plotting routine.

---
+++ plot/fillsinkComparisonPlot.m
@@ -0,0 +1,8 @@
+function [ plotHandle ] = fillsinkComparisonPlot(   demGRIDobj, ...
+                                                    fillsinkGRIDobj )
+%UNTITLED9 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
